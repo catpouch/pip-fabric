d491b8df6f7e9d94396c4a6511e2358ce8509215
CatSack <bossness544@gmail.com>
2022-10-18 21:12:30 -0700

    initial commit

---
+++ src/main/java/catpouch/pip/Pip.java
@@ -0,0 +1,10 @@
+package catpouch.pip;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Pip implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/catpouch/pip/client/PipClient.java
@@ -0,0 +1,13 @@
+package catpouch.pip.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class PipClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
