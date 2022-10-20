13c3375f9b0c9615fbc2b395726363e015d1b0ff
CatSack <bossness544@gmail.com>
2022-10-19 21:24:45 -0700

    the "hard part" done

--- src/main/java/catpouch/pip/Pip.java
+++ src/main/java/catpouch/pip/Pip.java
@@ -1,10 +1,30 @@
 package catpouch.pip;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
+import net.fabricmc.fabric.api.networking.v1.PlayerLookup;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
 
 public class Pip implements ModInitializer {
+    public interface PipConstants {
+        Identifier PING_PACKET_ID = new Identifier("pip", "ping_packet");
+    }
+
     @Override
     public void onInitialize() {
-
+        ServerPlayNetworking.registerGlobalReceiver(PipConstants.PING_PACKET_ID, (server, player, handler, buf, responseSender) -> {
+            BlockPos blockPos = buf.readBlockPos();
+            System.out.println(blockPos.toShortString());
+            PacketByteBuf returnBuf = PacketByteBufs.create();
+            returnBuf.writeBlockPos(blockPos);
+            for(ServerPlayerEntity onlinePlayer : PlayerLookup.all(server)) {
+                System.out.println(onlinePlayer.getName());
+                ServerPlayNetworking.send(onlinePlayer, PipConstants.PING_PACKET_ID, returnBuf);
+            }
+        });
     }
 }
--- src/main/java/catpouch/pip/client/PipClient.java
+++ src/main/java/catpouch/pip/client/PipClient.java
@@ -3,11 +3,67 @@ package catpouch.pip.client;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.text.LiteralText;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.hit.EntityHitResult;
+import net.minecraft.util.hit.HitResult;
+import net.minecraft.util.math.BlockPos;
+import org.lwjgl.glfw.GLFW;
 
 @Environment(EnvType.CLIENT)
 public class PipClient implements ClientModInitializer {
+    public interface PipClientConstants {
+        Identifier PING_PACKET_ID = new Identifier("pip", "ping_packet");
+    }
+
+
     @Override
     public void onInitializeClient() {
+        KeyBinding pingBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.pip.ping",
+                InputUtil.Type.MOUSE,
+                GLFW.GLFW_MOUSE_BUTTON_MIDDLE,
+                "key.category.pip.test"
+        ));
 
+        ClientPlayNetworking.registerGlobalReceiver(PipClientConstants.PING_PACKET_ID, (client, handler, buf, responseSender) -> {
+//            client.player.sendMessage(new LiteralText(buf.readBlockPos().toShortString()), false);
+            BlockPos target = buf.readBlockPos();
+            String message = target.toShortString();
+
+            client.execute(() -> {
+                client.player.sendMessage(new LiteralText(message), false);
+            });
+        });
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while(pingBinding.wasPressed()) {
+                HitResult hit = client.crosshairTarget;
+                PacketByteBuf buf = PacketByteBufs.create();
+                switch(hit.getType()) {
+                    case MISS:
+                        break;
+                    case BLOCK:
+                        BlockHitResult blockHit = (BlockHitResult) hit;
+                        BlockPos blockPos = blockHit.getBlockPos();
+                        buf.writeBlockPos(blockPos);
+                        ClientPlayNetworking.send(PipClientConstants.PING_PACKET_ID, buf);
+                        break;
+                    case ENTITY:
+                        EntityHitResult entityHit = (EntityHitResult) hit;
+                        BlockPos entityPos = entityHit.getEntity().getBlockPos();
+                        buf.writeBlockPos(entityPos);
+                        ClientPlayNetworking.send(PipClientConstants.PING_PACKET_ID, buf);
+                }
+            }
+        });
     }
 }
